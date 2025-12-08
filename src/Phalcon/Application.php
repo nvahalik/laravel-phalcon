99c9d6d0ae87f732d84231c88946a680b7f27118
Nick Vahalik <nick@nickvahalik.com>
2025-12-08 11:17:10 -0600

    Add Phalcon Application class and initialize in dispatcher
    This commit adds a complete Phalcon MVC Application class to bridge Laravel routing with Phalcon's application lifecycle. The Application class handles the full MVC request cycle including module initialization, router/dispatcher integration, view rendering, and response handling with configurable headers/cookies.
    
    Also initializes the Phalcon application in ControllerDispatcher with module registration and DI container setup, though the implementation appears incomplete as it registers modules but doesn't fully integrate with the existing dispatcher logic.
    
    Note: Contains debugging artifact 'x' character at src/Routing/ControllerDispatcher.php:49 that should be removed.

---
+++ src/Phalcon/Application.php
@@ -0,0 +1,459 @@
+<?php
+
+/**
+ * This file is part of the Phalcon Framework.
+ *
+ * (c) Phalcon Team <team@phalcon.io>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+namespace Vahalik\LaravelPhalcon\Phalcon;
+
+use Closure;
+use Phalcon\Application\AbstractApplication;
+use Phalcon\Http\ResponseInterface;
+use Phalcon\Mvc\Application\Exception;
+
+/**
+ * Phalcon\Mvc\Application
+ *
+ * This component encapsulates all the complex operations behind instantiating
+ * every component needed and integrating it with the rest to allow the MVC
+ * pattern to operate as desired.
+ *
+ *```php
+ * use Phalcon\Mvc\Application;
+ *
+ * class MyApp extends Application
+ * {
+ *     /**
+ *      * Register the services here to make them general or register
+ *      * in the ModuleDefinition to make them module-specific
+ *      *\/
+ *     protected function registerServices()
+ *     {
+ *
+ *     }
+ *
+ *     /**
+ *      * This method registers all the modules in the application
+ *      *\/
+ *     public function main()
+ *     {
+ *         $this->registerModules(
+ *             [
+ *                 "frontend" => [
+ *                     "className" => "Multiple\\Frontend\\Module",
+ *                     "path"      => "../apps/frontend/Module.php",
+ *                 ],
+ *                 "backend" => [
+ *                     "className" => "Multiple\\Backend\\Module",
+ *                     "path"      => "../apps/backend/Module.php",
+ *                 ],
+ *             ]
+ *         );
+ *     }
+ * }
+ *
+ * $application = new MyApp();
+ *
+ * $application->main();
+ *```
+ */
+class Application extends AbstractApplication
+{
+    /**
+     * @var bool
+     */
+    protected $implicitView = true;
+
+    /**
+     * @var bool
+     */
+    protected $sendCookies = true;
+
+    /**
+     * @var bool
+     */
+    protected $sendHeaders = true;
+
+    /**
+     * Handles a MVC request
+     *
+     * @param string $uri
+     * @return ResponseInterface|bool
+     */
+    public function handle(string $uri)
+    {
+        $container = $this->container;
+
+        if ($container === null) {
+            throw new Exception(
+                "A dependency injection container is required to access internal services"
+            );
+        }
+
+        $eventsManager = $this->eventsManager;
+
+        /**
+         * Call boot event, this allow the developer to perform initialization
+         * actions
+         */
+        if ($eventsManager !== null && $eventsManager->fire("application:boot", $this) === false) {
+            return false;
+        }
+
+        $router = $container->getShared("router");
+
+        /**
+         * Handle the URI pattern (if any)
+         */
+        $router->handle($uri);
+
+        /**
+         * If a 'match' callback was defined in the matched route
+         * The whole dispatcher+view behavior can be overridden by the developer
+         */
+        $matchedRoute = $router->getMatchedRoute();
+
+        if (is_object($matchedRoute)) {
+            $match = $matchedRoute->getMatch();
+
+            if ($match !== null) {
+                if ($match instanceof Closure) {
+                    $match = Closure::bind($match, $container);
+                }
+
+                /**
+                 * Directly call the match callback
+                 */
+                $possibleResponse = call_user_func_array(
+                    $match,
+                    $router->getParams()
+                );
+
+                /**
+                 * If the returned value is a string return it as body
+                 */
+                if (is_string($possibleResponse)) {
+                    $response = $container->getShared("response");
+
+                    $response->setContent($possibleResponse);
+
+                    return $response;
+                }
+
+                /**
+                 * If the returned string is a ResponseInterface use it as
+                 * response
+                 */
+                if (is_object($possibleResponse) && $possibleResponse instanceof ResponseInterface) {
+                    $possibleResponse->sendHeaders();
+                    $possibleResponse->sendCookies();
+
+                    return $possibleResponse;
+                }
+            }
+        }
+
+        /**
+         * If the router doesn't return a valid module we use the default module
+         */
+        $moduleName = $router->getModuleName();
+
+        if (!$moduleName) {
+            $moduleName = $this->defaultModule;
+        }
+
+        $moduleObject = null;
+
+        /**
+         * Process the module definition
+         */
+        if ($moduleName) {
+            if (is_object($eventsManager)) {
+                if ($eventsManager->fire("application:beforeStartModule", $this, $moduleName) === false) {
+                    return false;
+                }
+            }
+
+            /**
+             * Gets the module definition
+             */
+            $module = $this->getModule($moduleName);
+
+            /**
+             * A module definition must be an array or an object
+             */
+            if (!is_array($module) && !is_object($module)) {
+                throw new Exception("Invalid module definition");
+            }
+
+            /**
+             * An array module definition contains a path to a module definition
+             * class
+             */
+            if (is_array($module)) {
+                /**
+                 * Class name used to load the module definition
+                 */
+                if (isset($module["className"])) {
+                    $className = $module["className"];
+                } else {
+                    $className = "Module";
+                }
+
+                /**
+                 * If developer specify a path try to include the file
+                 */
+                if (isset($module["path"])) {
+                    $path = $module["path"];
+
+                    if (!file_exists($path)) {
+                        throw new Exception(
+                            "Module definition path '" . $path . "' doesn't exist"
+                        );
+                    }
+
+                    if (!class_exists($className, false)) {
+                        require_once $path;
+                    }
+                }
+
+                $moduleObject = $container->get($className);
+
+                /**
+                 * 'registerAutoloaders' and 'registerServices' are
+                 * automatically called
+                 */
+                $moduleObject->registerAutoloaders($container);
+                $moduleObject->registerServices($container);
+            } else {
+                /**
+                 * A module definition object, can be a Closure instance
+                 */
+                if (!($module instanceof Closure)) {
+                    throw new Exception("Invalid module definition");
+                }
+
+                $moduleObject = call_user_func_array(
+                    $module,
+                    [
+                        $container
+                    ]
+                );
+            }
+
+            /**
+             * Calling afterStartModule event
+             */
+            if (is_object($eventsManager)) {
+                $eventsManager->fire("application:afterStartModule", $this, $moduleObject);
+            }
+        }
+
+        /**
+         * Check whether use implicit views or not
+         */
+        $implicitView = $this->implicitView;
+
+        if ($implicitView === true) {
+            $view = $container->getShared("view");
+        }
+
+        /**
+         * We get the parameters from the router and assign them to the dispatcher
+         * Assign the values passed from the router
+         */
+        $dispatcher = $container->getShared("dispatcher");
+
+        $dispatcher->setModuleName($router->getModuleName());
+        $dispatcher->setNamespaceName($router->getNamespaceName());
+        $dispatcher->setControllerName($router->getControllerName());
+        $dispatcher->setActionName($router->getActionName());
+        $dispatcher->setParams($router->getParams());
+
+        /**
+         * Start the view component (start output buffering)
+         */
+        if ($implicitView === true) {
+            $view->start();
+        }
+
+        /**
+         * Calling beforeHandleRequest
+         */
+        if (is_object($eventsManager)) {
+            if ($eventsManager->fire("application:beforeHandleRequest", $this, $dispatcher) === false) {
+                return false;
+            }
+        }
+
+        /**
+         * The dispatcher must return an object
+         */
+        $controller = $dispatcher->dispatch();
+
+        /**
+         * Get the latest value returned by an action
+         */
+        $possibleResponse = $dispatcher->getReturnedValue();
+
+        /**
+         * Returning false from an action cancels the view
+         */
+        if ($possibleResponse === false) {
+            $response = $container->getShared("response");
+        } else {
+            /**
+             * Returning a string makes use it as the body of the response
+             */
+            if (is_string($possibleResponse)) {
+                $response = $container->getShared("response");
+
+                $response->setContent($possibleResponse);
+            } else {
+                /**
+                 * Check if the returned object is already a response
+                 */
+                $returnedResponse = (is_object($possibleResponse) && ($possibleResponse instanceof ResponseInterface));
+
+                /**
+                 * Calling afterHandleRequest
+                 */
+                if (is_object($eventsManager)) {
+                    $eventsManager->fire("application:afterHandleRequest", $this, $controller);
+                }
+
+                /**
+                 * If the dispatcher returns an object we try to render the view
+                 * in auto-rendering mode
+                 */
+                if ($returnedResponse === false && $implicitView === true) {
+                    if (is_object($controller)) {
+                        $renderStatus = true;
+
+                        /**
+                         * This allows to make a custom view render
+                         */
+                        if (is_object($eventsManager)) {
+                            $renderStatus = $eventsManager->fire("application:viewRender", $this, $view);
+                        }
+
+                        /**
+                         * Check if the view process has been treated by the
+                         * developer
+                         */
+                        if ($renderStatus !== false) {
+
+                            /**
+                             * Automatic render based on the latest controller
+                             * executed
+                             */
+                            $view->render(
+                                $dispatcher->getControllerName(),
+                                $dispatcher->getActionName()
+                            );
+                        }
+                    }
+                }
+
+                /**
+                 * Finish the view component (stop output buffering)
+                 */
+                if ($implicitView === true) {
+                    $view->finish();
+                }
+
+                if ($returnedResponse === true) {
+                    /**
+                     * We don't need to create a response because there is one
+                     * already created
+                     */
+                    $response = $possibleResponse;
+                } else {
+                    $response = $container->getShared("response");
+
+                    if ($implicitView === true) {
+                        /**
+                         * The content returned by the view is passed to the
+                         * response service
+                         */
+                        $response->setContent(
+                            $view->getContent()
+                        );
+                    }
+                }
+            }
+        }
+
+        /**
+         * Calling beforeSendResponse
+         */
+        if (is_object($eventsManager)) {
+            $eventsManager->fire("application:beforeSendResponse", $this, $response);
+        }
+
+        /**
+         * Check whether send headers or not (by default yes)
+         */
+        if ($this->sendHeaders) {
+            $response->sendHeaders();
+        }
+
+        /**
+         * Check whether send cookies or not (by default yes)
+         */
+        if ($this->sendCookies) {
+            $response->sendCookies();
+        }
+
+        /**
+         * Return the response
+         */
+        return $response;
+    }
+
+    /**
+     * Enables or disables sending cookies by each request handling
+     *
+     * @param bool $sendCookies
+     * @return Application
+     */
+    public function sendCookiesOnHandleRequest(bool $sendCookies): Application
+    {
+        $this->sendCookies = $sendCookies;
+
+        return $this;
+    }
+
+
+    /**
+     * Enables or disables sending headers by each request handling
+     *
+     * @param bool $sendHeaders
+     * @return Application
+     */
+    public function sendHeadersOnHandleRequest(bool $sendHeaders): Application
+    {
+        $this->sendHeaders = $sendHeaders;
+
+        return $this;
+    }
+
+    /**
+     * By default. The view is implicitly buffering all the output
+     * You can full disable the view component using this method
+     *
+     * @param bool $implicitView
+     * @return Application
+     */
+    public function useImplicitView(bool $implicitView): Application
+    {
+        $this->implicitView = $implicitView;
+
+        return $this;
+    }
+}
--- src/Routing/ControllerDispatcher.php
+++ src/Routing/ControllerDispatcher.php
@@ -9,58 +9,181 @@ class ControllerDispatcher extends \Illuminate\Routing\ControllerDispatcher
 {
     public function dispatch(Route $route, $controller, $method)
     {
+        if (! $route->action['phalconMetadata']) {
+            return parent::dispatch($route, $controller, $method);
+        }
+
+        require '/code/vendor/autoload.php';
+
         $modules = config('phalcon.modules', []);
 
-        // If a module was defined, go find it.
-        if (isset($route->action['phalconMetadata']['module'])) {
-            try {
-                $modClass = new $modules[$route->action['phalconMetadata']['module']]['className'];
-                $modClass->registerServices(\Phalcon\Di\Di::getDefault());
-            } catch (\Exception $e) {
-                if (!config('phalcon.runtime.ignore_missing_modules', false)) {
-                    throw $e;
-                }
+        $phalcon = app(\Vahalik\LaravelPhalcon\Phalcon\Application::class);
+
+        $phalcon->registerModules($modules);
+        $di = \Phalcon\Di\FactoryDefault::getDefault();
+        $phalcon->setDI($di);
+
+        $this->setUpPhalconEnvironment();
+
+        $phalcon->setEventsManager($di->getShared('eventsManager'));
+
+        $phalcon->prepareDispatcherWithRoute(function ($dispatcher) use ($route) {
+            $metadata = $route->action['phalconMetadata'];
+            $dispatcher->setModuleName($metadata['module']);
+            $dispatcher->setNamespaceName($metadata['namespace']);
+            $dispatcher->setControllerName($metadata['controller']);
+            $dispatcher->setActionName($metadata['action']);
+            // If there are "hard coded parameters" we merge those in.
+
+            $params = $route->parameters();
+            $onlyParams = collect($route->action['phalconMetadata'])->except('controller', 'action', 'module', 'namespace')->toArray();
+            if (count($onlyParams) > count($params)) {
+                $params = array_merge($params, array_slice($onlyParams, count($params)));
             }
-        }
 
-        if (method_exists($controller, 'beforeExecuteRoute')) {
-            $controller->setDi(\Phalcon\Di\Di::getDefault());
-            $controller->beforeExecuteRoute(\Phalcon\Di\Di::getDefault()->get('dispatcher'));
-        }
+            $dispatcher->setParams($params);
+        });
 
-        $result = parent::dispatch($route, $controller, $method);
+        $phalcon->setModuleName($route->action['phalconMetadata']['module']);
 
-        if (method_exists($controller, 'afterExecuteRoute')) {
-            $controller->afterExecuteRoute(\Phalcon\Di\Di::getDefault()->get('dispatcher'));
-        }
-
-        if (!$result && \Phalcon\Di\Di::getDefault()->has('view')) {
-            $view = \Phalcon\Di\Di::getDefault()->getShared('view');
-            $view->start();
-            $controller = request()->route()->getAction()['controller'];
-            preg_match('/\\\\(\w+)Controller@(\w+)Action/', $controller, $matches);
-
-            $view->render(strtolower($matches[1]), $matches[2]);
-            $view->finish();
-
-            return $view->getContent();
-        }
+        // Up to this...
+        $result = $phalcon->handle($route->uri());
 
         if ($result instanceof \Phalcon\Http\ResponseInterface) {
-            return new Response($result->getContent(), $result->getStatusCode(), $result->getHeaders()->toArray());
+            if (!$result->hasHeader('Status')) {
+                $statusCode = 200;
+            } else {
+                $statusCode = (int)$result->getHeaders()->get('Status');
+            }
+            if ($statusCode == '0') {
+            }
+            return new Response($result->getContent(), $statusCode, $result->getHeaders()->toArray());
         }
 
         return $result;
     }
 
-    protected function resolveParameters(Route $route, $controller, $method)
+//    protected function resolveParameters(Route $route, $controller, $method)
+//    {
+//        if (empty($route->action['phalconMetadata'])) {
+//            return parent::resolveParameters($route, $controller, $method);
+//        }
+//
+//        $base = $route->parametersWithoutNulls();
+//
+//        return array_merge($base, array_slice($route->action['phalconMetadata'], count($base)));
+//    }
+
+    public function setUpPhalconEnvironment()
     {
-        if (empty($route->action['phalconMetadata'])) {
-            return parent::resolveParameters($route, $controller, $method);
+        $di = \Phalcon\Di\FactoryDefault::getDefault();
+
+        $host = request()->getHost();
+        $subdomain = explode('.', $host)[0];
+
+        if ($subdomain === 'admin') {
+            return;
         }
 
-        $base = $route->parametersWithoutNulls();
+        // Lookup Account from Subdomain
+        $accountLookupQuery = new \Rentvine\Core\Account\Queries\AccountFromSubdomain([
+            'subdomain' => $subdomain,
+        ]);
 
-        return array_merge($base, array_slice($route->action['phalconMetadata'],  count($base)));
+        if (!$accountLookupQuery->execute()) {
+            $response = new \Phalcon\Http\Response();
+
+            $response->setStatusCode(404);
+            $response->setContent('No Account Found');
+
+            return $response->send();
+        }
+
+        // Get Account Data
+        $accountData = $accountLookupQuery->results;
+
+        if (!$accountData) {
+            $response = new \Phalcon\Http\Response();
+
+            $response->setStatusCode(404);
+            $response->setContent('No Account Found');
+
+            return $response->send();
+        }
+
+        // Check if Subdomain is Primary (redirect if not)
+        $isPrimarySubdomain = \Rentvine\ArrayHelper::getValue($accountData, 'isPrimarySubdomain', 1);
+
+        if (!$isPrimarySubdomain) {
+            $response = new \Phalcon\Http\Response();
+
+            $response->setStatusCode(202);
+            $response->setHeader('Content-Type', 'application/json');
+            $response->setContent(json_encode([
+                'message' => 'Subdomain is not primary',
+                'redirectTo' => \Rentvine\ArrayHelper::getValue($accountData, 'subdomain'),
+            ]));
+
+            return $response->send();
+        }
+
+        // Set Account Data
+        $account = new \Rentvine\Core\Account\Models\Accounts($accountData);
+
+        foreach (\Rentvine\Core\Account\Models\Accounts::JSON_FIELDS as $field) {
+            $value = $account->{$field};
+
+            if (!empty($value) && is_string($value)) {
+                $account->assign([
+                    $field => json_decode($value, true),
+                ]);
+            }
+        }
+
+        $di->set('account', function () use ($account) {
+            return $account;
+        }, true);
+
+        $eventsManager = \Phalcon\Di\FactoryDefault::getDefault()->get('eventsManager');
+        $eventsManager->attach('application:afterStartModule', function ($event, $application, $module) {
+        });
+
+        $eventsManager->attach('application:beforeHandleRequest', function ($event, $application) use ($di) {
+            $account = $di->get('account');
+            $config = $di->getShared('config');
+
+            $filesystem = $di->get('filesystem');
+            $factory = new \Rentvine\Filesystem\Factory();
+
+            // public account file system
+            $options = $config->filesystems->accountPublic->toArray();
+            $adapterName = \Rentvine\ArrayHelper::getValue($options, 'adapter');
+            unset($options['adapter']);
+            $publicAdapter = $factory->newInstance($adapterName, array_merge($options, [
+                'path' => $account->directory . DIRECTORY_SEPARATOR,
+            ]));
+            $publicFilesystem = new \League\Flysystem\Filesystem($publicAdapter);
+            $filesystem->dangerouslyMountFilesystems('public', $publicFilesystem);
+
+            // private account file system
+            $options = $config->filesystems->accountPrivate->toArray();
+            $adapterName = \Rentvine\ArrayHelper::getValue($options, 'adapter');
+            unset($options['adapter']);
+            $privateAdapter = $factory->newInstance($adapterName, array_merge($options, [
+                'path' => $account->directory . DIRECTORY_SEPARATOR,
+            ]));
+            $privateFilesystem = new \League\Flysystem\Filesystem($privateAdapter);
+            $filesystem->dangerouslyMountFilesystems('private', $privateFilesystem);
+
+            // exchange file system
+            $options = $config->filesystems->accountPrivate->toArray();
+            $adapterName = \Rentvine\ArrayHelper::getValue($options, 'adapter');
+            unset($options['adapter']);
+            $exchangeAdapter = $factory->newInstance($adapterName, array_merge($options, [
+                'path' => $account->directory . DIRECTORY_SEPARATOR . 'exchange' . DIRECTORY_SEPARATOR,
+            ]));
+            $exchangeFilesystem = new \League\Flysystem\Filesystem($exchangeAdapter);
+            $filesystem->dangerouslyMountFilesystems('exchange', $exchangeFilesystem);
+        });
     }
 }
